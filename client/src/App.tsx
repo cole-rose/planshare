49689e8375d559874d3eef10b794a6950971b014
Cole Rose <colerose@berkeley.edu>
2021-12-21 22:19:08 -0800

    install hotloader as dependency, update tsconfig.json to remove false warnings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-
+import { countReset } from "console";
+import React from "react";
+import { AppBar, Container , Toolbar, Typography} from "@material-ui/core";
 function App() {
   return (
-    <div className="App">
-          Learn React
-
-    </div>
-  );
+  <Container className="App">
+    <AppBar>
+      <Toolbar> 
+        <Typography variant="h4">
+          planshare
+        </Typography>
+      </Toolbar>
+  </AppBar>
+</Container>);
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+declare var module:any;
+if (module.hot) {
+  module.hot.accept();
+}
 ReactDOM.render(
   <>
     <App />
