44a6bbbd354340276695a3c518cff74a0f90f4e4
Cole Rose <colerose@berkeley.edu>
2021-12-22 18:06:07 -0800

    added some fields to login, modifying css to better match wireframes

--- client/src/components/LogIn/LogIn.tsx
+++ client/src/components/LogIn/LogIn.tsx
@@ -1,26 +1,31 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import {makeStyles} from '@mui/styles';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { makeStyles } from "@mui/styles";
+import { Box, Typography } from "@mui/material";
 const useStyles = makeStyles(() => {
-    return {
-      root: {
-  
-        "&.MuiButton-contained": {
-          color: "#fffff",
-          background: "#f57c00"
-        },
+  return {
+    root: {
+      "&.MuiButton-contained": {
+        color: "#fffff",
+        background: "#f57c00",
       },
-      siteName: {
-        color: "#f57c00"
-      }, 
-  
-    }});
+      "&.MuiDialogContentText-root": {
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "row",
+      },
+    },
+    siteName: {
+      color: "#f57c00",
+    },
+  };
+});
 export default function LogIn() {
   const [open, setOpen] = React.useState(false);
 
@@ -34,32 +39,47 @@ export default function LogIn() {
   const classes = useStyles();
   return (
     <div>
-      <Button className={classes.root}  variant= 'contained' onClick={handleClickOpen}>
+      <Button
+        className={classes.root}
+        variant="contained"
+        onClick={handleClickOpen}
+      >
         LogIn
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle className = {classes.siteName}>planshare</DialogTitle>
+        <DialogTitle className={classes.siteName}>planshare</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Welcome to planshare
+          {/* <Box display='flex' alignItems='center' justifyContent='center' flexDirection='row'> */}
+          <DialogContentText className={classes.root}>
+            <Typography>Welcome to </Typography> <Typography>&nbsp;</Typography>{" "}
+            <Typography className={classes.siteName}> planshare</Typography>
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="firstName"
-            label="First Name"
-            type="name"
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="lastName"
-            label="Last Name"
-            type="name"
-        
-            variant="standard"
-          />
+          {/* </Box> */}
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+          >
+            <Box display="flex" paddingRight={2}>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="firstName"
+                label="First Name"
+                type="name"
+                variant="standard"
+              />
+            </Box>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="lastName"
+              label="Last Name"
+              type="name"
+              variant="standard"
+            />
+          </Box>
           <TextField
             autoFocus
             margin="dense"
@@ -69,17 +89,17 @@ export default function LogIn() {
             fullWidth
             variant="standard"
           />
-        
-        <TextField
+
+          <TextField
             autoFocus
             margin="dense"
             id="password"
-            label="Password"
+            label="Create a Password"
             type="password"
             fullWidth
             variant="standard"
           />
-           <TextField
+          <TextField
             autoFocus
             margin="dense"
             id="password"
@@ -88,14 +108,20 @@ export default function LogIn() {
             fullWidth
             variant="standard"
           />
-          </DialogContent>
+        </DialogContent>
+
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Box display="flex" justifyContent="center">
+            <Button
+              className={classes.root}
+              onClick={handleClose}
+              variant="contained"
+            >
+              Create Account
+            </Button>
+          </Box>
         </DialogActions>
       </Dialog>
     </div>
   );
 }
-
-
